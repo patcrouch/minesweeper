fa73ac406cd984b23a38bc591eec2eeb44388e0d
patcrouch <pcrouch@alumni.nd.edu>
2020-12-10 17:07:40 -0800

    added all functionality to mine.py to remove unnecessary abstraction

--- mine.py
+++ mine.py
@@ -23,6 +23,12 @@ class Board:
         self.mine_num = mine_num
         self.graph = {}
         self.game_status = 0
+        self.mine_list = []
+        self.uncovered_list = []
+        self.flag_check = False
+        self.sweep_check = False
+        self.overlap_flag_check = False
+        self.overlap_sweep_check = False
     
     def set_graph(self, y_click, x_click):
         squares = [(y,x) for y in range(self.height) for x in range(self.width)]
@@ -63,6 +69,59 @@ class Board:
                 return
         else:
             return
+    
+    def flag(self):
+        self.flag_check = False
+        for square in self.graph.values():
+            if square.number == len([s.loc for s in square.adj if s.cover==True]) and square.number != 0 and square.cover == False:
+                for adj in [s for s in square.adj if s.cover == True]:
+                    if adj.flag == False:
+                        adj.flag = True
+                        self.flag_check = True
+
+    def sweep(self):
+        self.sweep_check = False
+        for square in self.graph.values():
+            if square.number == len([s.loc for s in square.adj if s.flag==True]) and square.number != 0 and square.cover == False:
+                for adj_loc in [s.loc for s in square.adj if s.flag==False]:
+                    if self.graph[adj_loc].cover == True:
+                        self.click(adj_loc[0],adj_loc[1])
+                        self.sweep_check = True
+    
+    def overlap_flag(self):
+        self.overlap_flag_check = False
+        for square in [s for s in self.graph.values() if s.number > 0 and s.cover == False]:
+            rem_mines = square.number - len([s.loc for s in square.adj if s.flag==True])
+            square_set = set([s for s in square.adj if s.cover==True and s.flag==False])
+            for adj in [s for s in square.adj if s.cover==False and s.number>0]:
+                adj_rem_mines = adj.number - len([s for s in adj.adj if s.flag==True])
+                adj_square_set = set([s for s in adj.adj if s.cover==True and s.flag==False])
+                if len(square_set.intersection(adj_square_set))>adj_rem_mines and len(square_set-adj_square_set)+adj_rem_mines==rem_mines:
+                    for s in square_set - adj_square_set:
+                        s.flag = True
+                        self.overlap_flag_check = True
+
+    def overlap_sweep(self):
+        self.overlap_sweep_check = False
+        for square in [s for s in self.graph.values() if s.number > 0 and s.cover == False]:
+            rem_mines = square.number - len([s.loc for s in square.adj if s.flag==True])
+            square_set = set([s for s in square.adj if s.cover==True and s.flag==False])
+            for adj in [s for s in square.adj if s.cover==False and s.number>0]:
+                adj_rem_mines = adj.number - len([s for s in adj.adj if s.flag==True])
+                adj_square_set = set([s for s in adj.adj if s.cover==True and s.flag==False])
+                if adj_rem_mines == rem_mines and adj_square_set.issubset(square_set):
+                    for s in square_set - adj_square_set:
+                        self.click(s.loc[0], s.loc[1])
+                        self.overlap_sweep_check = True
+    
+    def random_click(self):
+        covered = [s for s in self.graph if self.graph[s].cover==True and self.graph[s].flag==False]
+        loc = random.choice(covered)
+        self.click(loc[0],loc[1])
+
+    def check_win(self):
+        if len([s for s in self.graph.values() if s.flag==True]) == self.mine_num:
+            self.game_status = 2
 
     def print_graph(self):
         df = pd.DataFrame(index=range(self.height), columns=range(self.width))
@@ -75,12 +134,3 @@ class Board:
         for s in self.graph.values():
             df.loc[s.loc[0],s.loc[1]] = s.cover
         print(df)
-'''
-b = Board(5,5,2)
-b.set_graph()
-b.click(4,4)
-b.print_graph()
-
-for i in b.graph:
-    print(b.graph[i])
-'''
