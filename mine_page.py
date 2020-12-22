326e485a60a25f559d3227a80e533263b7a69ca7
patcrouch <pcrouch@alumni.nd.edu>
2020-12-21 23:15:15 -0800

    min and max values set for height, width, mine_num, and start_loc so errors are avoived

--- mine_page.py
+++ mine_page.py
@@ -16,8 +16,12 @@ class MinePage(tk.Frame):
         self.start_loc.set('0,0')
         self.height = tk.IntVar()
         self.height.set(16)
+        self.min_height = 4
+        self.max_height = 16
         self.width = tk.IntVar()
         self.width.set(16)
+        self.min_width = 8
+        self.max_width = 30
         self.mine_num = tk.IntVar()
         self.mine_num.set(40)
         self.random_click = (0,0)
@@ -69,87 +73,56 @@ class MinePage(tk.Frame):
         
         self.board_canvas = tk.Canvas(self, bg='white smoke', height=self.board.height*25,width=self.board.width*25)
 
-    def set_can_squares(self):
-        for y in range(self.board.height):
-            for x in range(self.board.width):
-                pos = self.get_pos(y,x)
-                if self.board.graph[(y,x)].cover and not self.board.graph[(y,x)].flag:
-                    self.board_canvas.create_polygon(pos, fill='gray')
-
-    def initiate_canvas(self):
-        for y in range(self.board.height):
-            for x in range(self.board.width):
-                pos = self.get_pos(y,x)
-                self.board_canvas.create_polygon(pos, fill='gray')
-
-    def set_flags(self):
-        for loc in self.to_flag:
-            pos = self.get_pos(loc[0],loc[1])
-            self.board_canvas.create_polygon(pos, fill='blue')
-        self.to_flag = set()
-    
-    def set_clicked(self):
-        for loc in self.to_click:
-            y = loc[0]
-            x = loc[1]
-            pos = self.get_pos(y,x)
-            text_pos = x*25+15, y*25+15
-            self.board_canvas.create_polygon(pos, fill='white')
-            if self.board.graph[loc].number > 0:
-                self.board_canvas.create_text(text_pos,text=str(self.board.graph[(y,x)].number),anchor=tk.CENTER, font=('arial black',9), fill='gray20')
-        self.to_click = set()
-
     def step(self):
         if self.board.game_status != 0:
             self.reset()
             self.board.game_status = 0
             return
         if self.step_count == 0:
-            self.board = Board(self.height.get(),self.width.get(),self.mine_num.get())
-            loc = self.start_loc.get().split(',')
-            self.to_click = self.board.set_graph(int(loc[0]),int(loc[1]))
-            self.board_canvas = tk.Canvas(self, bg='gray82', height=self.board.height*25+2,width=self.board.width*25+2)
-            self.board_canvas.grid(row=0,column=8,rowspan=4,padx=4)
             self.initiate_canvas()
-            self.set_clicked()
-            self.update_log(f"{self.step_count}: Initial Click")
         else:
             self.algorithm_operations()
-        self.board.check_win()
-        if self.board.game_status == 1:
-            self.update_log('GAME OVER')
-            for mine in self.board.mine_set:
-                y = mine[0]
-                x = mine[1]
-                if not self.board.graph[(y,x)].flag:
-                    pos = self.get_pos(y,x)
-                    if mine == self.random_click:
-                        self.board_canvas.create_polygon(pos, fill='gray12')
-                    else:
-                        self.board_canvas.create_polygon(pos, fill='red')
-                
-        elif self.board.game_status == 2:
-            self.update_log('CONGRATS!')
-            self.to_flag = self.board.mine_set
-        self.set_flags()
+        self.win_loss_scenarios()
+
+    def initiate_canvas(self):
+        height = self.height.get()
+        if height > self.max_height:
+            self.height.set(self.max_height)
+        if height < self.min_height:
+            self.height.set(self.min_height)
+        width = self.width.get()
+        if width > self.max_width:
+            self.width.set(self.max_width)
+        if width < self.min_width:
+            self.width.set(self.min_width)
+        mine_num = self.mine_num.get()
+        max_mines = int(height*width/4.8)
+        if mine_num > max_mines:
+            self.mine_num.set(max_mines)
+        if mine_num < 1:
+            self.mine_num.set(1)
+        self.board = Board(self.height.get(),self.width.get(),self.mine_num.get())
+        loc = self.start_loc.get().split(',')
+        loc = (int(loc[0]),int(loc[1]))
+        if loc[0] > self.board.height:
+            loc = (self.board.height-1,loc[1])
+        if loc[0] < 0:
+            loc = (0,loc[1])
+        if loc[1] > self.board.width:
+            loc = (loc[0],self.board.width-1)
+        if loc[1] < 0:
+            loc = (loc[0],0)
+        self.start_loc.set(f"{loc[0]},{loc[1]}")
+        self.to_click = self.board.set_graph(loc[0],loc[1])
+        self.board_canvas = tk.Canvas(self, bg='gray82', height=self.board.height*25+2,width=self.board.width*25+2)
+        self.board_canvas.grid(row=0,column=8,rowspan=4,padx=4)
+        for y in range(self.board.height):
+            for x in range(self.board.width):
+                pos = self.get_pos(y,x)
+                self.board_canvas.create_polygon(pos, fill='gray')
         self.set_clicked()
-        self.step_count += 1
-        self.board_list.append(copy.deepcopy(self.board))
-
-
-    def step_back(self):
-        if len(self.board_list) > 1:
-            self.step_log.configure(state = tk.NORMAL)
-            if self.board.game_status == 0:
-                self.step_log.delete('end-2l', 'end-1c')
-            else:
-                self.step_log.delete('end-3l', 'end-1c')
-            self.step_log.configure(state = tk.DISABLED)
-            self.board_list.pop()
-            self.board = copy.deepcopy(self.board_list[-1])
-            self.set_can_squares()
-            self.step_count -= 1
-
+        self.update_log(f"{self.step_count}: Initial Click")
+ 
     def algorithm_operations(self):
         tc = self.board.sweep()
         if self.board.sweep_check:
@@ -173,6 +146,65 @@ class MinePage(tk.Frame):
             return
         self.random_click, self.to_click = self.board.random_click()
         self.update_log(f"{self.step_count}: Random Click")
+
+    def win_loss_scenarios(self):
+        self.board.check_win()
+        if self.board.game_status == 1:
+            self.update_log('GAME OVER')
+            for mine in self.board.mine_set:
+                y = mine[0]
+                x = mine[1]
+                if not self.board.graph[(y,x)].flag:
+                    pos = self.get_pos(y,x)
+                    if mine == self.random_click:
+                        self.board_canvas.create_polygon(pos, fill='gray12')
+                    else:
+                        self.board_canvas.create_polygon(pos, fill='red')
+               
+        elif self.board.game_status == 2:
+            self.update_log('CONGRATS!')
+            self.to_flag = self.board.mine_set
+        self.set_flags()
+        self.set_clicked()
+        self.step_count += 1
+        self.board_list.append(copy.deepcopy(self.board))
+
+    def set_flags(self):
+        for loc in self.to_flag:
+            pos = self.get_pos(loc[0],loc[1])
+            self.board_canvas.create_polygon(pos, fill='blue')
+        self.to_flag = set()
+    
+    def set_clicked(self):
+        for loc in self.to_click:
+            y = loc[0]
+            x = loc[1]
+            pos = self.get_pos(y,x)
+            text_pos = x*25+15, y*25+15
+            self.board_canvas.create_polygon(pos, fill='white')
+            if self.board.graph[loc].number > 0:
+                self.board_canvas.create_text(text_pos,text=str(self.board.graph[(y,x)].number),anchor=tk.CENTER, font=('arial black',9), fill='gray20')
+        self.to_click = set()
+
+    def step_back(self):
+        if len(self.board_list) > 1:
+            self.step_log.configure(state = tk.NORMAL)
+            if self.board.game_status == 0:
+                self.step_log.delete('end-2l', 'end-1c')
+            else:
+                self.step_log.delete('end-3l', 'end-1c')
+            self.step_log.configure(state = tk.DISABLED)
+            self.board_list.pop()
+            self.board = copy.deepcopy(self.board_list[-1])
+            self.reset_squares()
+            self.step_count -= 1
+
+    def reset_squares(self):
+        for y in range(self.board.height):
+            for x in range(self.board.width):
+                pos = self.get_pos(y,x)
+                if self.board.graph[(y,x)].cover and not self.board.graph[(y,x)].flag:
+                    self.board_canvas.create_polygon(pos, fill='gray')
 
     def get_pos(self,y,x):
         return x*25+4, y*25+4, x*25+4, (y+1)*25+2, (x+1)*25+2, (y+1)*25+2, (x+1)*25+2, y*25+4
